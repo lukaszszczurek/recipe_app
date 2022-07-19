fbb569dbb19c9422dd9ab0fa39ee328de8133de6
Lukasz <lukaszszczurek02@gmial.com>
2022-07-19 21:09:21 +0200

    fetch api console log completed

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Popular(props) {
+
+    // const [popular,setPopular]=useState([]);
+
+    useEffect(()=>{
+        getPopular();
+    },[]);
+
+    const getPopular=async ()=>{
+        const api=await fetch('https://api.spoonacular.com/recipes/random?apiKey=ec2df15c2abd45769b40f8c7cad212b5&number=9');
+        const data= await api.json();
+        console.log(data);
+
+    }
     return (
         <div>
             Popular
+            {/*{popular.map(recipe =>{*/}
+            {/*    <p key={recipe.id}>*/}
+            {/*        {recipe.title}*/}
+            {/*    </p>*/}
+
+
+            {/*})}*/}
         </div>
     );
 }
